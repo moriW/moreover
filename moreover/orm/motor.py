cda5f5c3e05de76dbdde4be72a2ef5b1d2392fea
moridisa <moridevelopers@gmail.com>
2022-09-02 04:28:19 +0000

    fix bug

--- moreover/orm/motor.py
+++ moreover/orm/motor.py
@@ -7,10 +7,11 @@
 #
 
 import bson
+from tornado.util import ObjectDict
 from pymongo import ReturnDocument
 from schema import Schema
 from moreover.base.logger import gen_logger
-from typing import Dict, List, Tuple, Union, Any
+from typing import Dict, List, Tuple, Union
 from motor import MotorClient, MotorDatabase, MotorCollection
 from motor.core import (
     AgnosticClient,
@@ -86,7 +87,7 @@ class MotorMeta(type):
         return new_cls
 
 
-class Collection(dict, metaclass=MotorMeta):
+class Collection(ObjectDict, metaclass=MotorMeta):
     db: MotorDatabase
     client: MotorClient
     collection: MotorCollection
@@ -103,25 +104,6 @@ class Collection(dict, metaclass=MotorMeta):
 
     def validate(self):
         self.schema.validate(self)
-
-    def __getattribute__(self, __name: str) -> Any:
-        if hasattr(self, __name):
-            return super().__getattribute__(__name)
-        return self[__name]
-
-
-    def __setattr__(self, __name: str, __value: Any) -> None:
-        self[__name] = __value
-        return super().__setattr__(__name, __value)
-
-    # def __setattr__(self, __name: str, __value: Any) -> None:
-    #     return super().__setattr__(__name, __value)
-
-    # def update_document(self, **kwargs):
-    #     if self.schema:
-    #         self.__document = self.schema.validate({**self.__document, **kwargs})
-    #     else:
-    #         self.__document.update(kwargs)
 
     async def save(self):
         if "_id" in self.__document and self.__document["_id"] is not None:
